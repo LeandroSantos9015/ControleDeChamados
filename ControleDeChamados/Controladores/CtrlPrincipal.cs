c19b5030bcac159fa643838037c6186376a15c33
Leandro Andrade <leandrosantos_11@hotmail.com>
2020-09-18 09:35:51 -0300

    Inicialização do projeto de controle de chamados

---
+++ ControleDeChamados/Controladores/CtrlPrincipal.cs
@@ -0,0 +1,29 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using View;
+
+namespace Controladores
+{
+    public class CtrlPrincipal
+    {
+        public ITelaPrincipal Principal { get; set; }
+
+
+        public CtrlPrincipal()
+        {
+            this.Principal = new FrmPrincipal();
+
+            this.DelegarEventos();
+
+        }
+
+        private void DelegarEventos()
+        {
+            
+        }
+    }
+}
---
+++ ControleDeChamados/ControleDeChamados/Program.cs
@@ -0,0 +1,26 @@
+﻿using Controladores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleDeChamados
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            CtrlPrincipal principal = new CtrlPrincipal();
+
+            Application.Run(principal.Principal.PrincipalView);
+        }
+    }
+}
